780fdeffa75b7eb4a8ec0b0894649c492d30826c
Brian Clozel <bclozel@vmware.com>
2023-06-15 18:44:39 +0200

    Fix context propagation

--- src/main/java/com/example/micrometertracingissue/MicrometerTracingIssueApplication.java
+++ src/main/java/com/example/micrometertracingissue/MicrometerTracingIssueApplication.java
@@ -1,5 +1,7 @@
 package com.example.micrometertracingissue;
 
+import reactor.core.publisher.Hooks;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicrometerTracingIssueApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(MicrometerTracingIssueApplication.class, args);
 	}
 }
